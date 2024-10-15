99ceb57c563af10d65cc0375bb35d1098ed5b24c
aksingh <abhisheksingh@myn.global>
2024-10-15 21:00:24 +0530

    Dropped to 1 worker 1 thread

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,6 +1,6 @@
 import os
 
-workers = int(os.environ.get('GUNICORN_PROCESSES', '2'))
+workers = int(os.environ.get('GUNICORN_PROCESSES', '1'))
 threads = int(os.environ.get('GUNICORN_THREADS', '1'))
 timeout = int(os.environ.get('GUNICORN_TIMEOUT', '500'))
 bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:8080')
