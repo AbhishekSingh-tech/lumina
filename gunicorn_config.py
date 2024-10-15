6e62f0fceed47f6728ad410973fa8132aaf98bc0
aksingh <abhisheksingh@myn.global>
2024-10-15 16:09:40 +0530

    Changed the worker count to hard coded 32

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,7 +1,6 @@
 import os
-import multiprocessing
 
-workers = int(os.environ.get('GUNICORN_PROCESSES', multiprocessing.cpu_count()))
+workers = int(os.environ.get('GUNICORN_PROCESSES', '32'))
 threads = int(os.environ.get('GUNICORN_THREADS', '7'))
 timeout = int(os.environ.get('GUNICORN_TIMEOUT', '120'))
 bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:8080')
